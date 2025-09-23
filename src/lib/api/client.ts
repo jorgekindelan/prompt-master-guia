314958e66b58b02bb934f3cec033270e1a14ed4c
jorgekindelan <113096401+jorgekindelan@users.noreply.github.com>
2025-09-23 17:36:30 +0200

    Update client.ts with real url

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE || 'http://127.0.0.1:8000/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE || 'https://iat-backend-1.onrender.com/api';
 
 // Create axios instance
 const apiClient = axios.create({
